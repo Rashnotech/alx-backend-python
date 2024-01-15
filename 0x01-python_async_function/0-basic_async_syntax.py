0cdf772d833662863d33994a8107f07de9d4cbbf
Rashnotech <rashnotech@gmail.com>
2024-01-15 23:43:52 +0000

    Fixed: Async function in python

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a module that implements asynchronous coroutine"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    A function that implements asynchronous coroutine
+    Args:
+        max_delay: params for waiting time
+    Returns:
+        a floating point value
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""a module that implements async routine"""
+import asyncio
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> list:
+    """
+    A function that list delays in asc order without using
+    sort.
+    Args:
+        n: an integer value for the n times spawn
+        max_delay: an integer value for delay
+    Returns:
+        a List of delays in asc order without sort
+    """
+    delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
+    return sorted(delays)
