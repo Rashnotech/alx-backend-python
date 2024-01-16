656a9e330ae99d539fadfff506f9f0e4074066ac
Rashnotech <rashnotech@gmail.com>
2024-01-16 00:19:19 +0000

    Fixed: concurrency routines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 """a module that implements async routine"""
 import asyncio
+from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     A function that list delays in asc order without using
     sort.
