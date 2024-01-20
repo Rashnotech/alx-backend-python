7383e35429c7722c5c91355bbff31e0a8e5f2939
Rashnotech <rashnotech@gmail.com>
2024-01-20 13:58:39 +0000

    Fixed: return type

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,14 +5,15 @@ import time
 from typing import TypeVar
 
 
-
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 T = TypeVar('T')
+
+
 def task_wait_random(max_delay: int) -> T:
     """
-    a Function that takes a maximum delay time 
+    A Function that takes a maximum delay time
     Args:
         max_delay: an integer value
     Returns:
