4082ec70f654f9c6574346744cfe30fe04362108
Rashnotech <rashnotech@gmail.com>
2024-01-20 14:55:25 +0000

    Fixed: return type annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,13 +2,13 @@
 """ a module that use comprehensions """
 import asyncio
 import random
-from typing import Generator
+from typing import List
 
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List[float]:
     """
     A function that collect 10 random number
     Return:
