06736467f0320bd199c0e6e947372cc1e7e3680f
Rashnotech <rashnotech@gmail.com>
2024-01-20 15:15:20 +0000

    Running job in parallel

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""a module that measure run time"""
+import asyncio
+from typing import Generator
+import time
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    A function that measure run time in async comprehension
+    """
+    start_time = time.time()
+    await asyncio.gather(
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension())
+    end_time = time.time()
+    return end_time - start_time
