4ba259398d1cdc4aea249e38fe4666da9973dc3c
Rashnotech <rashnotech@gmail.com>
2024-02-10 16:17:34 +0000

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """a module that parameterize and patch as decorators"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -30,7 +30,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
     
     @patch('client.get_json')
-    @patch('client.GithubOrgClient.org', new_callable=unittest.mock.PropertyMock)
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
     def test_public_repos_url(self, mock_org, mock_get_json):
         """Test public repos url method"""
         mocked_payload = {'repos_url': 'https://github.com/orgs/abc/repos'}
@@ -44,6 +44,28 @@ class TestGithubOrgClient(unittest.TestCase):
 
         # Assertion
         self.assertEqual(actual_url, mocked_payload['repos_url'])
+
+    
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient._public_repos_url', new_callable=PropertyMock)
+    def test_public_repos(self, mock_org, mock_get_json):
+        """
+        Test public repos method
+        """
+        mocked_payload = [
+                {'name': 'repo1', 'license': {'key': 'MIT'}},
+                {'name': 'repo2', 'license': {'key': 'Apache'}}
+                ]
+        mock_org.return_value = 'https://api.github.com/orgs/abc/repos'
+        mock_get_json.return_value = mocked_payload
+
+        client = GithubOrgClient('abc')
+        result = client.public_repos()
+
+        expected = ['repo1', 'repo2']
+        self.assertEqual(result, expected)
+        mock_org.assert_called_once()
+        mock_get_json.assert_called_once_with('https://api.github.com/orgs/abc/repos')
 
 
 if __name__ == '__main__':
