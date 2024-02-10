63c748959d55590cfd4d48ea67c60b9465817163
Rashnotech <rashnotech@gmail.com>
2024-02-10 15:00:50 +0000

    Mocking property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -28,6 +28,23 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(client.org, mock_response)
         mock_get_json.assert_called_once_with(expected_url)
 
+    
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient.org', new_callable=unittest.mock.PropertyMock)
+    def test_public_repos_url(self, mock_org, mock_get_json):
+        """Test public repos url method"""
+        mocked_payload = {'repos_url': 'https://github.com/orgs/abc/repos'}
+        mock_org.return_value = mocked_payload
+
+        # Create an instance of the client
+        client = GithubOrgClient('abc')
+
+        # Call the method under test
+        actual_url = client._public_repos_url
+
+        # Assertion
+        self.assertEqual(actual_url, mocked_payload['repos_url'])
+
 
 if __name__ == '__main__':
     unittest.main()
