7e0cf50dcfd5371af7b6dc990e70ef0fc57c0b8f
Rashnotech <rashnotech@gmail.com>
2024-02-11 09:54:08 +0000

    Integration test: fixtures

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """a module that parameterize and patch as decorators"""
 import unittest
-from unittest.mock import patch, PropertyMock
-from parameterized import parameterized
+from unittest.mock import patch, PropertyMock, Mock
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -77,6 +78,57 @@ class TestGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient('abc')
         result = client.has_license(mock_license, mock_key)
         self.assertEqual(result, expected)
+
+@parameterized_class([
+    {
+        'org_payload': TEST_PAYLOAD[0][0],
+        'repos_payload': TEST_PAYLOAD[0][1],
+        'expected_repos': TEST_PAYLOAD[0][2],
+        'apache2_repos': TEST_PAYLOAD[0][3],
+        }
+    ])
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """
+    A class that handle integration testing
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        """setup"""
+        payload = {
+                'https://api.github.com/orgs/google': cls.org_payload,
+                'https://api.github.com/orgs/google/repos': cls.repos_payload
+                }
+
+        def get_payload(url):
+            if url in payload:
+                return Mock(**{'json.return_value': payload[url]})
+            return HTTPError
+
+        cls.get_patcher = patch('requests.get', side_effect=get_payload)
+        cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        """Tear Down"""
+        cls.get_patcher.stop()
+
+
+    def test_public_repos(self):
+        """An integration test"""
+        org_client = GithubOrgClient('google')
+        result = org_client.public_repos()
+
+        self.assertEqual(result, self.expected_repos)
+
+
+    def test_public_repos_with_license(self):
+        # Create an instance of GithubOrgClient
+        org_client = GithubOrgClient('google')
+        result = org_client.public_repos(license='apache-2.0')
+
+        self.assertEqual(result, self.apache2_repos)
+
 
 
 if __name__ == '__main__':
